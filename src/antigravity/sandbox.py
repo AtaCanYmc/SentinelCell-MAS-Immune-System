8fea30ccadf06f136d02b3d56fee80008abe6bce
Ata Can <atacanymc@gmail.com>
2026-06-22 22:49:32 +0300

    chore: Remove unused consumer, producer, and sandbox files

--- src/antigravity/sandbox.py
+++
@@ -1,8 +0,0 @@
-from src.core.logger import get_console
-
-console = get_console()
-
-
-class Sandbox:
-    def execute(self, code: str):
-        console.print("[hack][Sandbox][/hack] Executing isolated code...")
